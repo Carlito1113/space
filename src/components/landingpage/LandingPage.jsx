e0eb00fbab1bdba4018a4ae5a2b5094145773061
Carlito Algarin <carlito1113@yahoo.com>
2021-07-23 19:53:18 -0400

    changed some styling and converted css to the BEM convention

--- src/components/landingpage/LandingPage.jsx
+++ src/components/landingpage/LandingPage.jsx
@@ -4,17 +4,17 @@ import './landingpage.css';
 export default function LandingPage() {
   return (
     <div className="header">
-      <div className="logo-container">
-        <h1 className="logo">Carlito Algarin</h1>
+      <div className="header__logo-container">
+        <h1 className="header__logo">{`{C.A}`}</h1>
       </div>
 
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Design</span>
-          <span className="heading-primary-sub">if you are looking for style, the search is over.</span>
+          <span className="heading-primary--main">Design</span>
+          <span className="heading-primary--sub">if you are looking for style, the search is over.</span>
         </h1>
 
-        <a href="#" className='btn btn-white btn-animated'>Whoa! a BuTTon!</a>
+        <a href="#" className='btn btn--white btn--animated'>My Works</a>
       </div>
     </div>
   );
